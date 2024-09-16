a40a38c2ac5f3b76227ede57ee2cf02d0eda2df3
elikya <elikyabokanga@gmail.com>
2024-09-16 15:00:44 -0500

    limited range of numbers to 100 using exp formula

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import { jogoDoDadoRNG } from "./rng.js";
 
-let numberOfTests = 100000
-let resultArray = []
+let numberOfTests = 250000
 
+let startDate = Date.now()
+let buckets = {
+  "a": 0,
+  "b": 0,
+  "c": 0,
+  "d": 0,
+  "e": 0,
+  "f": 0
+}
 for (let i=0; i<numberOfTests; i++) {
-  resultArray.push(jogoDoDadoRNG())
+  const result = jogoDoDadoRNG()
+  if (result == 100) {buckets["a"]++}
+  else if (result < 200) {buckets["b"]++}
+  else if (result < 1000) {buckets["c"]++}  
+  else if (result < 5000) {buckets["d"]++}
+  else if (result < 10000) {buckets["e"]++}
+  else {buckets["f"]++}
 }
 
-console.log(resultArray)
+for (let k of Object.keys(buckets)) {
+  buckets[k] = buckets[k]/numberOfTests*100
+}
+
+console.log(buckets)
+console.log(Date.now()-startDate)
--- rng.js
+++ rng.js
@@ -3,7 +3,7 @@ import crypto from "crypto";
 let clientSeed =
   "000000000000000007a9a31ef7f07463d91af6a5454241d5fbf282e5f0fe1b3a";
 
-function genServerSeed() {
+export function genServerSeed() {
   return crypto.randomBytes(256).toString("hex");
 }
 
@@ -12,12 +12,12 @@ function genServerSeed() {
  * @param {string} serverSeed
  * @returns {string} 
  */
-function genGameHash(serverSeed) {
+export function genGameHash(serverSeed) {
   return crypto.createHash("sha256").update(serverSeed).digest("hex");
 }
 
 
-function divisible(hash, mod) {
+export function divisible(hash, mod) {
   // We will read in 4 hex at a time, but the first chunk might be a bit smaller
   // So ABCDEFGHIJ should be chunked like  AB CDEF GHIJ
   let val = 0;
@@ -35,22 +35,26 @@ function divisible(hash, mod) {
  * @param {string} serverSeed
  * @returns {number}
  */
-function genCrashPointFromHash(serverSeed) {
-  let hash = crypto
+export function genCrashPointFromHash(serverSeed) {
+  const min = 100
+  const max = 10000
+  const hash = crypto
     .createHmac("sha256", serverSeed)
     .update(clientSeed)
     .digest("hex");
 
   // In 1 of 101 games the game crashes instantly.
-  if (divisible(hash, 98)) return 100;
+  if (divisible(hash, 98)) return min;
   // Use the most significant 52 bits from the hash to calculate the crash point
-  let h = parseInt(hash.slice(0, 52 / 4), 16);
-  let e = Math.pow(2, 52);
-
-  return Math.min(Math.floor((100 * e - h) / (e - h)), 10000); // 100 max value
+  const h = parseInt(hash.slice(0, 52 / 4), 16);
+  const e = Math.pow(2, 52);
+  const r = h/e // random number
+  const k = 5 // decay constant
+  // console.log(Math.exp(-k*r))
+  return min + Math.ceil((max-min)*(Math.exp(-k*r)))
 }
 
 
 export function jogoDoDadoRNG () {
-  return genCrashPointFromHash(genGameHash(genServerSeed()))/100
+  return genCrashPointFromHash(genGameHash(genServerSeed()))
 }
